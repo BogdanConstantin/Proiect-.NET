f33dfe0081066bbcde4074379beebd7a7d3d6171
Catalin <mihai.catalin197@gmail.com>
2018-12-20 23:59:12 +0200

    Write Controllers & Repositories for Managements

---
+++ Proiect Back-End/BusinessLogic.ClassesManagement/Write/Implementations/CourseManagementLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.ClassesManagement.Write.Implementations
+{
+    using BusinessLogic.ClassesManagement.Write.Abstractions;
+
+    using DataAccess.ClassesManagement.Write.Abstractions;
+
+    using Entities.ClassesManagement;
+
+    using Models.ClassesManagement;
+
+    public class CourseManagementLogic : BaseLogic, ICourseManagementLogic
+    {
+        public CourseManagementLogic(IRepository repository)
+            : base(repository)
+        {
+        }
+
+        public void Create(ManagementDto courseDto)
+        {
+            var newCourse = new CourseManagement()
+            {
+                ClassId = courseDto.ClassId,
+                EntityId = Guid.NewGuid(),
+                AuthorId = Guid.NewGuid(),
+                UserId = courseDto.UserId,
+                UserPosition = courseDto.UserPosition
+                // load managed course when read is done
+            };
+
+            _repository.Insert(newCourse);
+            _repository.Save();
+        }
+    }
+}
---
+++ Proiect Back-End/ClassesManagement/Controllers/CourseManagementWriteController.cs
@@ -0,0 +1,29 @@
+﻿
+namespace ClassesManagement.Controllers
+{
+    using BusinessLogic.ClassesManagement.Write.Abstractions;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Models.ClassesManagement;
+
+    [Route("api/course/manage")]
+    [ApiController]
+    public class CourseManagementWriteController : ControllerBase
+    {
+        private readonly ICourseManagementLogic _managementLogic;
+
+        public CourseManagementWriteController(ICourseManagementLogic managementLogic)
+        {
+            _managementLogic = managementLogic;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ManagementDto managementDto)
+        {
+            _managementLogic.Create(managementDto);
+
+            return Ok(managementDto);
+        }
+    }
+}
