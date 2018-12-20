2325e0118d8f4c6d72b7420bd5ed2d6a0d992e80
Catalin <mihai.catalin197@gmail.com>
2018-12-21 01:53:52 +0200

    Update & Delete on Management Controllers

--- Proiect Back-End/BusinessLogic.ClassesManagement/Abstractions/ILaboratoryManagementLogic.cs
+++ Proiect Back-End/BusinessLogic.ClassesManagement/Abstractions/ILaboratoryManagementLogic.cs
@@ -2,9 +2,14 @@
 namespace BusinessLogic.ClassesManagement.Abstractions
 {
     using Models.ClassesManagement;
+    using System;
 
     public interface ILaboratoryManagementLogic
     {
-        void Create(ManagementDto courseManagementDto);
+        void Create(ManagementDto laboratoryManagementDto);
+
+        void Update(ManagementDto laboratoryManagementDto, Guid laboratoryManagementEntityId);
+
+        void Delete(Guid laboratoryManagementEntityId);
     }
 }
