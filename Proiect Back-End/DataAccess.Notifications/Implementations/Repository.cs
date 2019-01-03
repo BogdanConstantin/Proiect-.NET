15e713c9c01ba160524081978c83552b2f2f6c4f
androbianca <biaanca.9805@yahoo.com>
2019-01-03 23:55:41 +0200

    repo

---
+++ Proiect Back-End/DataAccess.Notifications/Implementations/Repository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Notifications.Abstractions;
+using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Notifications.Implementations
+{
+    public class Repository : IRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void Insert<T>(T entity)
+            where T : BaseEntity
+        {}
+
+        public T GetLastByFilter<T>(Expression<Func<T, bool>> filter)
+            where T : BaseEntity
+        {
+            return null;
+
+        }
+
+        public ICollection<T> GetAll<T>() where T : BaseEntity
+        {
+            return null;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
